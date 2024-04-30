fdf30a0cadd5b5c9821230dc9abf3ad008a934b4
Diego <die.bri.19@gmail.com>
2024-04-30 13:31:41 -0500

    un server hecho a lo tradicional

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-
-import { envs } from './config/env.js'
-
-//const { startServer } = require('./server/server')
-import { startServer } from './server/server.js'
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
 
 
 const main = () => {
     startServer({
-        port : envs.PORT,
+        port: envs.PORT,
         public_path: envs.PUBLIC_PATH
     })
 }
 
-(async ()=> {
+// Función agnóstica autoconvocada
+// Agnóstica por que no tiene nombre
+// Autoconvocada porque la ejecutamos con los parentesis
+(async () => {
     main()
-}) ()
+})()
--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,26 @@
-import express from 'express'
+const express = require('express')
+const path = require('path')
 
-//const path = require('path')
-import path from 'path'
+const startServer = (options) => {
+    const { port, public_path = 'public' } = options
 
-export const startServer = (options) => {
-    const { port, public_path = 'public'} = options
-    
     const app = express()
-    app.use(express.static(public_path))
 
-    app.get ('*',(req,res) => {
+    // Para poder usar middlewares se usa la palabra use (express)
+    app.use(express.static(public_path)) // contenido estático que ponemos disponible
+
+    app.get('*', (req, res) => {
         const indexPath = path.join(__dirname + `../../../${public_path}/clases-presenciales.html`)
         res.sendFile(indexPath)
     })
 
     app.listen(port, () => {
         console.log(`Escuchando en el puerto ${port}`)
-    
     })
 
 }
 
+
+module.exports = {
+    startServer
+}
